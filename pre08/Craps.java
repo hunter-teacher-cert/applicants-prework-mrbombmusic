f477928575ff2246f861a05578406b49c65d419b
Liam Baum <liamcbaum@gmail.com>
2021-06-28 21:29:36 -0400

    added more detailed comments

--- pre08/Craps.java
+++ pre08/Craps.java
@@ -6,12 +6,14 @@ import java.util.Scanner;
 
 public class Craps{
 
+// die roll method
   public static int roll(int sides) {
     Random dice = new Random();
     int diceRoll = dice.nextInt(sides) + 1;
     return diceRoll;
   }
 
+// total value of multiple dice rolls method
   public static int shoot(int numDice, int maxValue) {
     int rollResult = 0;
     for(int i = 0; i < numDice; i++) {
@@ -20,23 +22,28 @@ public class Craps{
     return rollResult;
   }
 
+/*
+This method is set up to return a 0 if the round is lost
+and a 1 if the round is won.
+The returned value is then used in a conditional statement in the main method
+to print out if the round was won or lost
+*/
   public static int round() {
     int diceRoll = shoot(2, 6);
-    int result = 0;
-    if(diceRoll == 2 || diceRoll == 3 || diceRoll == 12) {
+    int result = 0; // initialize result variable
+    if(diceRoll == 2 || diceRoll == 3 || diceRoll == 12) {   // These roll values result in loss
       result = 0;
-    } else if(diceRoll == 7 || diceRoll == 11) {
+    } else if(diceRoll == 7 || diceRoll == 11) {   // These roll values result in win
       result = 1;
-    } else {
+    } else { // all other roll values are stored in new variable
       int point = diceRoll;
       diceRoll = shoot(2, 6);
-      while(diceRoll != point && diceRoll != 7) {
+      while(diceRoll != point && diceRoll != 7) { // loop continues to roll until point value or 7 is rolled
         diceRoll = shoot(2, 6);
-        //System.out.println("Roll Again");
       }
-      if(diceRoll == point) {
+      if(diceRoll == point) { // Rolled point value resulting in win
         result = 1;
-      } else if(diceRoll == 7) {
+      } else if(diceRoll == 7) { // Rolled 7 resulting in loss
         result = 0;
       }
     }
