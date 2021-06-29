2b7076ec7bc0203aa967f800dffaa016f756549a
Liam Baum <liamcbaum@gmail.com>
2021-06-28 20:24:49 -0400

    added more detailed comments

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -12,10 +12,30 @@ public class Loops{
   //   return term;
   // }
 
+/*
+This method checks the results of myexp function and Math.myexp.
+The exercise says to adjust the number of terms until the estimated value
+agrees with the correct answer when x is 1. That is the number 17 in the myexp.
+However, the exercise also said that the number of terms should be the second argument that check sends to myexp.
+But it did not specify to make the check method with two arguments.
+*/
   public static String check(double x) {
       return x + "\t" + myexp(x, 17) + "\t" + Math.exp(x);
   }
 
+
+/*
+e^x = 1 + x + x^2 / 2! + x^3 / 3! + x^4 / 4! + …
+It was not clear what 'e' was in this exercise.
+
+The instructions said
+"You can make this method more efficient if you realize that
+the numerator of each term is the same as its predecessor multiplied by x,
+and the denominator is the same as its predecessor multiplied by i"
+This is why I created the 2 previous variables, to store the values of
+the predecessor that would then be multiplied by x and i respectively.
+The argument n is the number of terms.
+*/
   public static double myexp(double x, int n) {
       double term = 0.0;
       double numerator = 1.0;
